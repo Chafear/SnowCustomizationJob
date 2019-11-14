9add93dfa560a72f87d4402c8bd9b357e9887a60
Chafear <mellenio05@gmail.com>
2019-11-14 22:10:50 +0300

    menu scene update

---
+++ Assets/_Scripts/UI/PlayerName.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PlayerName : MonoBehaviour
+{
+    public TextMeshProUGUI playerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var name = GameObject.FindGameObjectWithTag("DATA").GetComponent<PlayerNameData>().Get();
+        if(name == "")
+        {
+            name = METADATA.PLAYER_NAME_PLACEHOLDER;
+        }
+        playerName.text = name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Scripts/Utility/METADATA.cs
+++ Assets/_Scripts/Utility/METADATA.cs
@@ -3,4 +3,5 @@ public static class METADATA
 {
     public static string HELMETS_PATH = "Items/Helmets";
     public static string SKIES_PATH = "Items/Skies";
+    public static string PLAYER_NAME_PLACEHOLDER = "НИКАК";
 }
