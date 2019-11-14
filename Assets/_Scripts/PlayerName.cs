212333d9f466dc023ddfb9b6c03b4a17855f25d3
Chafear <mellenio05@gmail.com>
2019-11-14 21:34:56 +0300

    Clean up

---
+++ Assets/_Scripts/PlayerName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+    public TMPro.TMP_InputField inputName;
+    public string text;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void SetName()
+    {
+        text = inputName.text;
+    }
+}
